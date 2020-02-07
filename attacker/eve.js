eb5116e92c7f432d0d488ddd384a31252785b96c
Dion Koeze <dj.koeze@avans.nl>
2020-02-07 14:14:42 +0100

    silence CORSE warnings

--- attacker/eve.js
+++ attacker/eve.js
@@ -6,6 +6,13 @@ const port = 4000;
 // parse the data from the url
 app.use(bodyParser.urlencoded({extended: false}));
 
+// prevent CORS warnings in browser console
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // on this endpoint we receive your secret password
 app.post('/', function(req, res) {
     console.log(req.body.key);
